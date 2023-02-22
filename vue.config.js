bb8586510d7ba0dee4adafb92fd98d7756871de2
Jacob Li <jacob.yq.lee@gmail.com>
2023-02-22 10:00:39 -0800

    Remove public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/trip-cost-calculator-app/",
+  // publicPath: "/trip-cost-calculator-app/",
 });
