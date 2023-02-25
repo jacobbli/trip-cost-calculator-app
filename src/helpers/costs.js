2ce4e186090ee446822578e83a52bfa62b6e1faa
Jacob Li <jacob.yq.lee@gmail.com>
2023-02-24 18:00:58 -0800

    Fix rate calculations

--- src/helpers/costs.js
+++ src/helpers/costs.js
@@ -1,23 +1,28 @@
 function calculateTripCost(tripDuration) {
+  const billableMinutes =
+    tripDuration.hours + Math.floor(tripDuration.minutes / 36) < 6
+      ? tripDuration.minutes < 36
+        ? tripDuration.minutes
+        : 0
+      : 0;
+
+  const billableHours =
+    tripDuration.hours + Math.floor(tripDuration.minutes / 36) < 6
+      ? tripDuration.hours + Math.floor(tripDuration.minutes / 36)
+      : 0;
+
+  const billableDays =
+    tripDuration.days +
+    (tripDuration.hours + Math.floor(tripDuration.minutes / 36) < 6 ? 0 : 1);
+
   const dayCost =
-    parseFloat(process.env.VUE_APP_DEFAULT_DAY_RATE) * tripDuration.days +
-    (tripDuration.hours >= 6
-      ? parseFloat(process.env.VUE_APP_DEFAULT_DAY_RATE)
-      : 0);
+    parseFloat(process.env.VUE_APP_DEFAULT_DAY_RATE) * billableDays;
 
   const hourCost =
-    (tripDuration.hours < 6
-      ? parseFloat(process.env.VUE_APP_DEFAULT_HOUR_RATE) * tripDuration.hours
-      : 0) +
-    (tripDuration.minutes >= 36 && tripDuration.hours < 6
-      ? parseFloat(process.env.VUE_APP_DEFAULT_HOUR_RATE)
-      : 0);
+    parseFloat(process.env.VUE_APP_DEFAULT_HOUR_RATE) * billableHours;
 
   const minuteCost =
-    tripDuration.minutes < 36 && tripDuration.hours < 6
-      ? parseFloat(process.env.VUE_APP_DEFAULT_MINUTE_RATE) *
-        tripDuration.minutes
-      : 0;
+    parseFloat(process.env.VUE_APP_DEFAULT_MINUTE_RATE) * billableMinutes;
 
   return {
     days: dayCost,
@@ -39,9 +44,7 @@ function calculateDiscounts(isBcaaMember, tripCost) {
 
 function calculateTax(totalCost, pvrtCost, accessFeeCost) {
   return {
-    tripGst:
-      ((totalCost + pvrtCost) * parseFloat(process.env.VUE_APP_DEFAULT_GST)) /
-      100,
+    tripGst: (totalCost * parseFloat(process.env.VUE_APP_DEFAULT_GST)) / 100,
     tripPst: (totalCost * parseFloat(process.env.VUE_APP_DEFAULT_PST)) / 100,
     pvrtGst: (pvrtCost * parseFloat(process.env.VUE_APP_DEFAULT_GST)) / 100,
     accessFeeGst:
