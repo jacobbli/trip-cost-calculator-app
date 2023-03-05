cba55e3a34d36cf4c295fa722b5d09c84c70d87a
Jacob Li <jacob.yq.lee@gmail.com>
2023-03-05 14:58:17 -0800

    Convert string to float

--- src/components/CostCalculator.vue
+++ src/components/CostCalculator.vue
@@ -75,7 +75,7 @@ const discounts = computed(() =>
   calculateDiscounts(isBcaaMember.value, tripCost.value.tripCost)
 );
 const accessFee = computed(() =>
-  includeAccessFee.value ? process.env.VUE_APP_ACCESS_FEE : 0
+  includeAccessFee.value ? parseFloat(process.env.VUE_APP_ACCESS_FEE) : 0
 );
 const pvrtDays = computed(() => calculatePvrtDays(tripDuration.value));
 const pvrtCost = computed(() => calculatePvrtCost(pvrtDays.value));
