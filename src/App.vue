c54e634d7d34eb876322297249ec528887f3fb08
Auldry Concepcion <auldry.concepcion@bcaa.com>
2023-03-07 10:45:22 -0800

    Change color

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ body {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: red;
 
   display: flex;
   flex-direction: column;
